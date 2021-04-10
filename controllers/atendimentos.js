95813127d7c070af32de51618063f49297975ca6
Matheus Filho <matheusrodrigofilho@outlook.com>
2021-04-10 04:42:40 -0300

    Creating the connection with database

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,3 @@
-
-
-
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos GET'))
 
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const customExpress = require('./config/customExpress')
+const connection = require('./infra/connection')
 
-const app = customExpress();
-app.listen(3000, () => console.log('[Server running - 3000]'))
+
+connection.connect((erro) => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('[Start Connection]')
+        const app = customExpress();
+        app.listen(3000, () => console.log('[Server running - 3000]'))
+    }
+})
+
 

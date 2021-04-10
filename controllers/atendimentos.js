a7145b62171555d93bfb394af3af284dbc8438b9
Matheus Filho <matheusrodrigofilho@outlook.com>
2021-04-10 04:14:40 -0300

    Creating a post route and add body parser configs

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,5 +2,10 @@
 
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos'))
+    app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos GET'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Voce esta na rota de atendimentos realizando um post')
+    })
 }
